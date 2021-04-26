6bc1ae95a102258a755389984bf7cbf2e10a26fd
Marina Julia <marinajulia707@gmail.com>
2021-04-26 16:39:41 -0300

    Alternando jogadores

--- JogoDaVelha/Program.cs
+++ JogoDaVelha/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
 namespace JogoDaVelha {
+
     class Program {
+
 
         static void Main(string[] args) {
 
-            int jogador = 1;
             int jogada;
+            double classificacaoJogada = 0;
+            string escolhaIconeDoJogador;
+            string jogadaDaVez = "";
+
 
             Console.WriteLine("__" + "|" + "__" + "|" + "__");
             Console.WriteLine("__" + "|" + "__" + "|" + "__" + "  * *Jogo da velha * *");
@@ -20,23 +25,89 @@ namespace JogoDaVelha {
 
 
             Console.WriteLine("Escolha seu ícone:(X/O)");
-            string escolhaIcone = Console.ReadLine();
+            escolhaIconeDoJogador = Console.ReadLine();
 
-            if (escolhaIcone == "x" || escolhaIcone == "X") {
-                Console.WriteLine("O outro jogador será 'O'");
-                jogador = 1;
-            } else if (escolhaIcone == "o" || escolhaIcone == "O") {
-                Console.WriteLine("O outro jogador será o 'X'");
-                jogador = -1;
+
+            double EscolhaDoIcone(string escolhaIcone) {
+
+                if (escolhaIcone == "x" || escolhaIcone == "X") {
+                    Console.WriteLine("O outro jogador será 'O'\n");
+                    classificacaoJogada = 1;
+
+                }
+                else if (escolhaIcone == "o" || escolhaIcone == "O") {
+                    Console.WriteLine("O outro jogador será o 'X'");
+                    classificacaoJogada = -1;
+                }
+                else {
+                    Console.WriteLine("Caracter inválido");
+                }
+                return classificacaoJogada;
             }
 
-            string[,] matriz = new string[3, 3];
-            
-            for (int i = 0; i < 9; i++) {
 
-                Console.WriteLine("Jogador faça sua jogada:");
-                jogada = Convert.ToInt32(Console.ReadLine());
-                var teste = ConverterJogada(jogada);
+
+            string[,] ConverterJogada(int Jogada) {
+                string[,] matriz = new string[3, 3];
+
+
+                if (Jogada == 1) {
+                    matriz[0, 0] = jogadaDaVez;
+                }
+                else if (Jogada == 2) {
+                    matriz[0, 1] = jogadaDaVez;
+                }
+                else if (Jogada == 3) {
+                    matriz[0, 2] = jogadaDaVez;
+                }
+                else if (Jogada == 4) {
+                    matriz[1, 0] = jogadaDaVez;
+                }
+                else if (Jogada == 5) {
+                    matriz[1, 1] = jogadaDaVez;
+                }
+                else if (Jogada == 6) {
+                    matriz[1, 2] = jogadaDaVez;
+                }
+                else if (Jogada == 7) {
+                    matriz[2, 0] = jogadaDaVez;
+                }
+                else if (Jogada == 8) {
+                    matriz[2, 1] = jogadaDaVez;
+                }
+                else if (Jogada == 9) {
+                    matriz[2, 2] = jogadaDaVez;
+                }
+                else {
+                    Console.WriteLine("Jogada inválida");
+                }
+                return matriz;
+            }
+
+
+
+            for (
+                int i = 0; i < 9; i++) {
+                double retornoDaJogada = EscolhaDoIcone(escolhaIconeDoJogador);
+
+
+                if (retornoDaJogada == 1) {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("Faça a jogada do 'X'");
+                    jogada = Convert.ToInt32(Console.ReadLine());
+                    jogadaDaVez = "X";
+                    ConverterJogada(jogada);
+                    retornoDaJogada = -1;
+                }
+                if (retornoDaJogada == -1) {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("Faça a jogada do 'O'");
+                    jogada = Convert.ToInt32(Console.ReadLine());
+                    jogadaDaVez = "O";
+                    ConverterJogada(jogada);
+                    retornoDaJogada = 1;   
+                }
+
 
             }
 
