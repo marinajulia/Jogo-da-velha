cd36b5a218138d4f40a82e34b5a3d5d042ade575
Marina Julia <marinajulia707@gmail.com>
2021-04-25 23:18:35 -0300

    Primeiro commit

---
+++ JogoDaVelha/ConverterJogada.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JogoDaVelha {
+    class ConverterJogada {
+        public int jogada;
+        public int[] posicoesMatriz = new int[2];
+       
+
+       public int numerosMatriz(jogada) {
+            if (jogada == 1) {
+                posicoesMatriz[0] = 0;
+                posicoesMatriz[1] = 0;
+            } else if (jogada == 2) {
+                posicoesMatriz[0] = 0;
+                posicoesMatriz[1] = 1;
+            } else if (jogada == 3) {
+                posicoesMatriz[0] = 0;
+                posicoesMatriz[1] = 2;
+            } else if (jogada == 4) {
+                posicoesMatriz[0] = 1;
+                posicoesMatriz[1] = 0;
+            } else if (jogada == 5) {
+                posicoesMatriz[0] = 1;
+                posicoesMatriz[1] = 1;
+            } else if (jogada == 6) {
+                posicoesMatriz[0] = 1;
+                posicoesMatriz[1] = 2;
+            } else if (jogada == 7) {
+                posicoesMatriz[0] = 2;
+                posicoesMatriz[1] = 0;
+            } else if (jogada == 8) {
+                posicoesMatriz[0] = 2;
+                posicoesMatriz[1] = 1;
+            } else if (jogada == 9) {
+                posicoesMatriz[0] = 2;
+                posicoesMatriz[1] = 2;
+            } else {
+                Console.WriteLine("Jogada inválida");
+            }
+            return posicoesMatriz[0];
+
+        }
+    }
+}
---
+++ JogoDaVelha/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace JogoDaVelha {
+    class Program {
+
+        static void Main(string[] args) {
+
+            int jogador = 1;
+            int jogada;
+
+            Console.WriteLine("__" + "|" + "__" + "|" + "__");
+            Console.WriteLine("__" + "|" + "__" + "|" + "__" + "  * *Jogo da velha * *");
+            Console.WriteLine("  " + "|" + "  " + "|" + "\n");
+            Console.WriteLine(">> A primeira jogada será do jogador 'X'");
+            Console.WriteLine("Posições possíveis: 1|2|3");
+            Console.WriteLine("                    4|5|6");
+            Console.WriteLine("                    7|8|9\n");
+            Console.WriteLine("--------------------------------------------\n");
+            Console.WriteLine("**Vamos jogar!**\n");
+
+
+            Console.WriteLine("Escolha seu ícone:(X/O)");
+            string escolhaIcone = Console.ReadLine();
+
+            if (escolhaIcone == "x" || escolhaIcone == "X") {
+                Console.WriteLine("O outro jogador será 'O'");
+                jogador = 1;
+            } else if (escolhaIcone == "o" || escolhaIcone == "O") {
+                Console.WriteLine("O outro jogador será o 'X'");
+                jogador = -1;
+            }
+
+            string[,] matriz = new string[3, 3];
+            
+            for (int i = 0; i < 9; i++) {
+
+                Console.WriteLine("Jogador faça sua jogada:");
+                jogada = Convert.ToInt32(Console.ReadLine());
+                var teste = ConverterJogada(jogada);
+
+            }
+
+
+        }
+    }
+}
