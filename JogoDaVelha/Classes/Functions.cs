7b0d47159c65813454680f1e26539cde01a94c6d
Marina Julia <marinajulia707@gmail.com>
2021-04-30 17:02:35 -0300

    Organizando métodos

--- JogoDaVelha/Classes/Functions.cs
+++ JogoDaVelha/Classes/Functions.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace JogoDaVelha.Classes {
     public class Functions {
+
         public string Informacoes() {
             string iconeEscolhido ="";
             Console.WriteLine("__" + "|" + "__" + "|" + "__");
@@ -49,6 +50,183 @@ namespace JogoDaVelha.Classes {
         }
         public void MensagemEmpate() {
             Console.WriteLine("Houve um empate!");
+        }
+
+
+        void VerificarVencedor() {
+            double soma = 0;
+            string vencedor = "";
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+
+                    soma += matrizNumerica[i, j];
+                    if (soma == 3) {
+                        vencedor = "x";
+                        MensagemSucesso(vencedor);
+                        Environment.Exit(0);
+
+                    }
+                    else if (soma == -3) {
+                        vencedor = "o";
+                        MensagemSucesso(vencedor);
+                        Environment.Exit(0);
+                    }
+
+                }
+                soma = 0;
+
+            }
+
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+                    soma += matrizNumerica[j, i];
+                    if (soma == 3) {
+                        vencedor = "x";
+                        MensagemSucesso(vencedor);
+                        Environment.Exit(0);
+
+                    }
+                    else if (soma == -3) {
+                        vencedor = "o";
+                        MensagemSucesso(vencedor);
+                        Environment.Exit(0);
+
+                    }
+                }
+                soma = 0;
+
+            }
+
+
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+                    if (i + j == 2) {
+                        soma += matrizNumerica[i, j];
+                        if (soma == 3) {
+                            vencedor = "x";
+                            MensagemSucesso(vencedor);
+                            Environment.Exit(0);
+
+                        }
+                        else if (soma == -3) {
+                            vencedor = "o";
+                            MensagemSucesso(vencedor);
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+
+            }
+            soma = 0;
+
+
+            for (int i = 0; i < 3; i++) {
+
+                for (int j = 0; j < 3; j++) {
+                    if (i == j) {
+                        soma += matrizNumerica[i, j];
+                        if (soma == 3) {
+                            vencedor = "x";
+                            MensagemSucesso(vencedor);
+                            Environment.Exit(0);
+
+                        }
+                        else if (soma == -3) {
+                            vencedor = "o";
+                            MensagemSucesso(vencedor);
+                            Environment.Exit(0);
+                        }
+
+                    }
+
+                }
+
+            }
+
+            soma = 0;
+
+
+        }
+
+
+        void aplicar() {
+            var matriz = new string[3, 3];
+            var matrizNumerica = new double[3, 3];
+            string jogadaDaVez = "";
+            var funcoesMatriz = new FuncoesDeMatriz();
+            int jogada;
+            double retornoDaJogada = EscolhaDoIcone(Informacoes());
+            for (int i = 0; i < 9; i++) {
+
+                if (retornoDaJogada == 1) {
+                    Console.WriteLine("-------------------------------------------------------------------");
+                    Console.WriteLine("Faça a jogada do 'X'");
+                    jogada = Convert.ToInt32(Console.ReadLine());
+                    jogadaDaVez = "x";
+                    Matrizes matrizCompleta = new Matrizes();
+                    matrizCompleta.matriz = matriz;
+                    matrizCompleta.matrizNumerica = matrizNumerica;
+                    var passarParaCoordenada = funcoesMatriz.ConverterJogada(jogada, matrizCompleta, jogadaDaVez);
+                    retornoDaJogada = -1;
+                    string imprimeJogada = "";
+
+                    for (int l = 0; l < 3; l++) {
+
+                        for (int j = 0; j < 3; j++) {
+
+                            if (matriz[l, j] == null) {
+                                imprimeJogada += "|" + "-" + "|";
+                            }
+                            else {
+                                imprimeJogada += "|" + matriz[l, j] + "|";
+                            }
+
+
+                        }
+
+                        Console.WriteLine(imprimeJogada);
+
+
+                        imprimeJogada = "";
+
+
+                    }
+                }
+                else if (retornoDaJogada == -1) {
+                    Console.WriteLine("-------------------------------------------------------------------");
+                    Console.WriteLine("Faça a jogada do 'O'");
+                    jogada = Convert.ToInt32(Console.ReadLine());
+                    jogadaDaVez = "o";
+                    Matrizes matrizCompleta = new Matrizes();
+                    matrizCompleta.matriz = matriz;
+                    matrizCompleta.matrizNumerica = matrizNumerica;
+                    var passarParaCoordenada = funcoesMatriz.ConverterJogada(jogada, matrizCompleta, jogadaDaVez);
+                    retornoDaJogada = 1;
+                    string imprimeJogada = "";
+
+                    for (int l = 0; l < 3; l++) {
+
+                        for (int j = 0; j < 3; j++) {
+
+                            if (matriz[l, j] == null) {
+                                imprimeJogada += "|" + "-" + "|";
+                            }
+                            else {
+                                imprimeJogada += "|" + matriz[l, j] + "|";
+                            }
+                        }
+                        Console.WriteLine(imprimeJogada);
+
+                        imprimeJogada = "";
+
+                    }
+                }
+                VerificarVencedor();
+
+            }
         }
 
     }
